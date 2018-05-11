f4013529997ebcb55853e2cb5149a683906b264b
leegangsan <nouvin@certon.co.kr>
2018-05-11 16:37:56 +0900

    Contact 선택기능 구현 | React.js 기초개념 응용

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
+import ContactDetails from './ContactDetails';
 
 export default class Contact extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            keyword:'',
-            contactData: [{
-                name: 'Abet',
-                phone: '010-0000-0001'
-            }, {
-                name: 'Betty',
-                phone: '010-0000-0002'
-            }, {
-                name: 'Charlie',
-                phone: '010-0000-0003'
-            }, {
-                name: 'David',
-                phone: '010-0000-0004'
-            }]
+          selectedKey: -1,
+          keyword:'',
+          contactData: [{
+              name: 'Abet',
+              phone: '010-0000-0001'
+          }, {
+              name: 'Betty',
+              phone: '010-0000-0002'
+          }, {
+              name: 'Charlie',
+              phone: '010-0000-0003'
+          }, {
+              name: 'David',
+              phone: '010-0000-0004'
+          }]
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleChange(e){
@@ -30,6 +33,13 @@ export default class Contact extends React.Component {
         keyword : e.target.value
       });
       console.log(this.state.keyword);
+    }
+
+    handleClick(key){
+      this.setState({
+        selectedKey:key
+      });
+      console.log(key,'is selected');
     }
 
     render() {
@@ -41,7 +51,12 @@ export default class Contact extends React.Component {
                 .indexOf(this.state.keyword) > -1;
             });
             return data.map((contact, i) => {
-                return (<ContactInfo contact={contact} key={i}/>);
+                return (<ContactInfo
+                   contact={contact}
+                   key={i}
+                   onClick={()=> this.handleClick(i)}
+                   />
+                 );
             });
         };
 
@@ -55,6 +70,10 @@ export default class Contact extends React.Component {
                   onChange={this.handleChange}
                 />
                 <div>{mapToComponents(this.state.contactData)}</div>
+                <ContactDetails
+                  isSelected={this.state.selectedKey != -1}
+                  contact={this.state.contactData[this.state.selectedKey]}
+                />
             </div>
         );
     }
