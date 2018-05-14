2049a3e292d587ccad76ed68af24ab01faf6cb65
leegangsan <nouvin@certon.co.kr>
2018-05-14 17:46:34 +0900

    Contact 데이터 추가기능 구현 | 컴포넌트 응용

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDetails from './ContactDetails';
+import ContactCreate from'./ContactCreate';
 import update from 'react-addons-update';
 
 export default class Contact extends React.Component {
@@ -49,7 +50,7 @@ export default class Contact extends React.Component {
 
     handleCreate(contact){
       this.setState({
-        contactDate : update(this.state.contactData,{ $push : [contact] })
+        contactData : update(this.state.contactData,{ $push : [contact] })
       });
     }
 
@@ -107,6 +108,9 @@ export default class Contact extends React.Component {
                   isSelected={this.state.selectedKey != -1}
                   contact={this.state.contactData[this.state.selectedKey]}
                 />
+              <ContactCreate
+                onCreate={this.handleCreate}
+              />
             </div>
         );
     }
